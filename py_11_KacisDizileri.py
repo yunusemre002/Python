513c320f0d34497fdbadf0f56ce0fb5c89a8ca21
Yunus Emre Demir <41540426+yunusemre002@users.noreply.github.com>
2020-03-02 15:27:39 +0300

    \n   \\  \\\\  \t

---
+++ py_11_KacisDizileri.py
@@ -0,0 +1,6 @@
+
+print("Yunus\nDemir")   # \n bir alt satıra iner.
+print("Eğer \\ yazmak istiyorsak \\\\ iki kere yazmalıyız.") # 2 kere için 4 tane ...
+print("yunus\temre")   #\t bir tab kadar booşluk
+
+
