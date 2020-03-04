0e12730572893298059c19092306baad76fd75c7
Yunus Emre Demir <41540426+yunusemre002@users.noreply.github.com>
2020-03-04 14:29:33 +0300

    Update py_16_Tuple(demet).py

--- py_16_Tuple(demet).py
+++ py_16_Tuple(demet).py
@@ -10,8 +10,17 @@ print("Listede ekleme yapmak mümkündür : ", list1)
 tuple0 = ("sifirinci")  # elemandan sonra bir virgül koymazsak parantezleri işlem önceliği gibi algılar
                         # So, tuple1 deki gibi son elemandan sonra bir virgül bırakılmalı tuple olduğunu anlatmak için
                         #veya tuple2 deki tanımlama yapılabilir.
-tuple1 = ("birinci","b", "c", "d",)
+tuple1 = ("birinci","b", "c", "d","b")
 tuple2 = tuple("a")
+
 print("tuple0 : ", type(tuple0), tuple0)
-print("tuple0 : ",type(tuple1), tuple1)
-print("tuple0 : ",type(tuple2), tuple2)
+print("tuple1 : ",type(tuple1), tuple1)
+print("tuple2 : ",type(tuple2), tuple2)
+
+
+# 2 tane metodu var index ve count
+print("\ntuple1 de 'b' sayisi : ", tuple1.count("b"), sep="")
+print("tuple1 de 'b'nin yeri : ", tuple1.index("b"), sep="")
+
+
+
