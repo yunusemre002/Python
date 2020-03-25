c05e509695c88636b2736c067293654a335400f2
Yunus Emre Demir <41540426+yunusemre002@users.noreply.github.com>
2020-03-25 19:52:00 +0300

    dizi[5] , [:5] , [5:] , [::] , [::-1] , [::3]
    5. karakter :",str[5]
    [0-4] arası :",str[:5]
    [5-sonsuz arası :",str[5:]
    [0-sonsuz arası :",str[::]
    [0-sonsuz arası tersten :",str[::-1]
    [0-sonsuz arası 3'er 3'er :",str[::+3]

---
+++ py_27_String_attributes.py
@@ -0,0 +1,13 @@
+
+str = "abcçdefghıijklmn"
+
+print("\t\t\t\t",str)
+print("5. karakter :",str[5])
+print("[0-4] arası :",str[:5])
+print("[5-sonsuz arası :",str[5:])
+print("[0-sonsuz arası :",str[::])
+print("[0-sonsuz arası tersten :",str[::-1])
+print("[0-sonsuz arası düzden :",str[::+1])
+print("[0-sonsuz arası 2'şer 2'şer :",str[::+2])
+print("[0-sonsuz arası 3'er 3'er :",str[::+3])
+print("[0-sonsuz arası 5'er 5'er :",str[::+5])
