cc13d5a413faf0be8e7063e3671a221053704d7a
Yunus Emre Demir <41540426+yunusemre002@users.noreply.github.com>
2020-03-26 16:33:11 +0300

    5.bit_lenght(), 7.5.as_integer_retio() .real .imag
    (9).bit_length()   // 9'un bit uzunluğu : 4
    (8.5).as_integer_ratio()   // 8.5 Hangi iki sayının bölümüdür : 
    (7.0).is_integer())   // 7.0 Float ama tam sayı mı : True
    (12.4j ).real   //real : 12.0
    (12.4j).imag   //12.4j sanal : 4.0

---
+++ py_29_Number_Methods.py
@@ -0,0 +1,12 @@
+sayi = 9
+print("Bit uzunluğu :",  sayi.bit_length())
+print("8.5 Hangi iki sayının bölümüdür :",  (8.5).as_integer_ratio())
+
+sayi3= 7.0
+print("7.0", type(sayi3), " --Float ama tam sayı mı : ", sayi3.is_integer())
+
+sayi4 = 12+4j
+print(sayi4)
+print("12.4j real :", sayi4.real)
+print("12.4j sanal :", sayi4.imag)
+
