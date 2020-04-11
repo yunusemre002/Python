73a5b44480971297e2a7d5b094700b5732c97fad
yunusemre <yunus.emredemir222002@gmail.com>
2020-04-11 20:29:12 +0300

    Templates added

--- Django/blog/blog/urls.py
+++ Django/blog/blog/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from home.views import home_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+
+    path('post/', include('post.urls') ),
+
+
 ]
--- Django/blog/post/views.py
+++ Django/blog/post/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+def post_index(request):
+    return HttpResponse('<h>İndex Page</h>')
+
+def post_detail(request):
+    return HttpResponse('<h>Detail Page</h>')
+
+def post_create(request):
+    return HttpResponse('<h>Create Page</h>')
+
+def post_update(request):
+    return HttpResponse('<h>Update Page</h>')
+
+def post_delete(request):
+    return HttpResponse('<h>Delete Page</h>')
+
