2f5be6da528db0b412a7fd44b85e76046f527e34
yunusemre <yunus.emredemir222002@gmail.com>
2020-04-16 22:48:20 +0300

    Templates added
    Add base.html

--- Django/blog/post/urls.py
+++ Django/blog/post/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('index', post_index, name = "index"),
     path('<int:id>', post_detail, name ="detail"),
     path('create', post_create, name = "create"),
-    path('update', post_update, name = "update"),
-    path('delete', post_delete, name = "delete")
+    path('<int:id>/update', post_update, name = "update"),
+    path('<int:id>/delete', post_delete, name = "delete")
 ]
--- Django/blog/post/views.py
+++ Django/blog/post/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,HttpResponse, get_object_or_404
+from django.shortcuts import render,HttpResponse, get_object_or_404, HttpResponseRedirect, redirect
 from .models import Post
 from .forms import PostForms
+from django.contrib import messages
 
 
 def post_index(request):
@@ -17,36 +18,44 @@ def post_detail(request, id):
 
 
 def post_create(request):
-    #if request.method == "POST":
-    #   print("Request post")
+    # if request.method == "POST":
+    #    print("Request post")
 
     # iki senaryo var birinde boş form istemesi diğerindde dolu  formu submit etmesi
-    """if request.method == "POST":
-        # Formdan gelen bilgileri kaydet
+    """if request.method == "POST":        # Formdan gelen bilgileri kaydet
         form = PostForms(request.POST)
         if form.is_valid():
             form.save()
-    else:
-        # Formu kullanıcılara göster.
+    else:                                 # Formu kullanıcılara göster.
         form = PostForms()"""
 
     form = PostForms(request.POST or None)  # Eğer requestçpost varsa al yoksa boş bir post oluştur. Yukarıki if le aynı
     if form.is_valid():
-        form.save()
-
+        post = form.save()                  # eklenen veya güncellenen nesneyi geri döndürür.
+        messages.success(request, "Basarili bir şekilde oluşturdunuz.", extra_tags='mesaj-basarili')
+        return HttpResponseRedirect(post.get_absolute_url())       # ilgili formun detayına gitsin bu yüzden saveden dönen metodun
+                                                                    #adresini alıp redirecte gönderdim.
     context = {
         "form": form,
     }
-
     return render(request, 'post/form.html', context)
 
 
+def post_update(request, id):
+    post = get_object_or_404(Post, id=id)                      # Post detailden aldık postun deyatlarını döndürüyor.
+    form = PostForms(request.POST or None, instance=post)      # Post create den aldık  içine üsteki postu koyduk.
+    if form.is_valid():                                        # İnstance : örnek,durum,istek...
+        form.save()                                            # Eklenen veya güncellenen nesneyi geri döndürür.
+        messages.success(request, "Basarili bir şekilde düzenlediniz.", extra_tags='mesaj-basarili')
+        return HttpResponseRedirect(post.get_absolute_url())   # ilgili formun detayına gitsin bu yüzden saveden dönen metodun
 
+    context = {
+                "form": form,
+               }
+    return render(request, 'post/form.html', context)
 
-
-def post_update(request):
-    return HttpResponse('<h>Update Page</h>')
-
-def post_delete(request):
-    return HttpResponse('<h>Delete Page</h>')
+def post_delete(request, id):
+    post = get_object_or_404(Post, id=id)
+    post.delete()
+    return redirect('post:index')
 
